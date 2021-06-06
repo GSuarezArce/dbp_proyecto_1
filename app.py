4c8502bcf504346581f6141d70974a520332e595
Ereiclo <eric.bracamonte@utec.edu.pe>
2021-06-05 20:23:46 -0500

    Create app.py

---
+++ app.py
@@ -0,0 +1,31 @@
+from flask import Flask, render_template, request, redirect, url_for
+from flask_sqlalchemy import SQLAlchemy
+import json
+
+app = Flask(__name__)
+
+app.config["SQLALCHEMY_DATABASE_URI"]="postgresql://postgres:1234@localhost:5432/dbp"
+
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+db = SQLAlchemy(app)
+
+class Todo(db.Model):
+    __tablename__ = 'todos'
+    id = db.Column(db.Integer, primary_key=True)
+    description = db.Column(db.String(80), nullable=False)
+
+    def __repr__(self):
+        return f'<Todo: {self.id}, {self.description}>'
+
+db.create_all()
+
+
+@app.route('/')
+def index():
+    
+    return render_template('index.html', data=Todo.query.all())
+
+if __name__ == '__main__':
+    app.run(port=5002, debug=True)
+else:
+    print('using global variables from FLASK')
