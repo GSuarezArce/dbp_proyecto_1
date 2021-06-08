b7fec45e6d1f5432c33bd8494386c7a8ae61a3f1
DarKNeSsJuaN25 <juan.laredo@utec.edu.pe>
2021-06-07 19:50:43 -0500

    hola

--- app.py
+++ app.py
@@ -4,18 +4,19 @@ import json
 
 app = Flask(__name__)
 
-app.config["SQLALCHEMY_DATABASE_URI"]="postgresql://postgres:1234@192.168.1.18:5432/postgres"
+app.config["SQLALCHEMY_DATABASE_URI"]="postgresql://postgres:Softjuandius_25@localhost:5432/proyecto1"
 
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 db = SQLAlchemy(app)
 
 class Usuario(db.Model):
+    __tablename__ = 'usuario'
     id= db.Column(db.Integer,primary_key=True)
     nombre=db.Column(db.String(50),nullable=True)
     apellidos=db.Column(db.String(60),nullable=True)
     usuario=db.Column(db.String(70),nullable=True)
     contrasenia=db.Column(db.String(40),nullable=True)  
-    email=db.Column(db.string(100),nullable=True)
+    email=db.Column(db.String(100),nullable=True)
 
 
 
